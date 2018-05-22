a1797278858257ffcad8b9c462d972a4f7df6211
buckethead1986 <rspeckpfs@gmail.com>
2018-05-22 15:28:00 -0400

    color competition logic added. Speed, color change options added

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const gameBoard = document.getElementsByClassName("board")[0];
-const boardSizeButton = document.getElementById("board-size-button");
+const boardOptionsButton = document.getElementById("board-options-button");
 const startButton = document.getElementById("start-button");
+const showOptionsButton = document.getElementById("showOptions");
+const optionsDiv = document.getElementById("options");
 let gameWidth = document.getElementById("board-width");
 let gameHeight = document.getElementById("board-height");
+let liveColorChange = document.getElementById("live-color");
+// let deadColorChange = document.getElementById("dead-color");
+let speedChange = document.getElementById("speed");
 let boardDimension = document.getElementById("board-dimension");
 let boardSize = [40, 40];
 let keydown = "";
 let interval;
 let isRunning = false;
-let speed = 300;
+let speed = 200;
 let tempBoard = [];
+let liveColor = "grey";
+let deadColor = "black";
 
 //starts everything
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,20 +27,12 @@ document.addEventListener("DOMContentLoaded", () => {
 //holder function for various document level event listeners
 const documentEventListeners = () => {
   //listens to board size input and resizes based on the values
-  boardSizeButton.addEventListener("click", e => {
+  boardOptionsButton.addEventListener("click", e => {
     e.preventDefault();
-    if (gameWidth.value === "") {
-      gameWidth.value = boardSize[0];
-    }
-    if (gameHeight.value === "") {
-      gameHeight.value = boardSize[1];
-    }
-    createBoard(gameWidth.value, gameHeight.value);
-    gameWidth.value = "";
-    gameHeight.value = "";
+    boardOptions(e);
   });
 
-  //'c' and 'x', when held, allow for multiple selection of cells as you mouse over them. (c, all to yellow, x, all to green)
+  //'c' and 'x', when held, allow for multiple selection of cells as you mouse over them. (c, all to liveColor, x, all to deadColor)
   document.addEventListener("keydown", e => {
     keydown = e.which;
   });
@@ -43,6 +42,42 @@ const documentEventListeners = () => {
 
   //starts the game loop
   startButton.addEventListener("click", addInterval);
+
+  //additional options hide/show toggle
+  showOptionsButton.addEventListener("click", () => {
+    if (optionsDiv.style.display === "block") {
+      optionsDiv.style.display = "none";
+    } else if (optionsDiv.style.display === "none") {
+      optionsDiv.style.display = "block";
+    }
+  });
+};
+
+//changes cell colors, speed, board size
+const boardOptions = e => {
+  if (gameWidth.value === "") {
+    gameWidth.value = boardSize[0];
+  }
+  if (gameHeight.value === "") {
+    gameHeight.value = boardSize[1];
+  }
+  if (gameWidth.value != boardSize[0] || gameHeight.value != boardSize[1]) {
+    createBoard(gameWidth.value, gameHeight.value);
+  }
+  if (liveColorChange.value !== liveColor && liveColorChange.value !== "") {
+    liveColor = liveColorChange.value;
+  }
+  // if (deadColorChange.value !== deadColor && deadColorChange.value !== "") {
+  //   deadColor = deadColorChange.value;
+  // }
+  if (speedChange.value !== speed && speedChange.value !== "") {
+    speed = speedChange.value;
+  }
+  liveColorChange.value = "";
+  // deadColorChange.value = "";
+  gameWidth.value = "";
+  gameHeight.value = "";
+  speedChange.value = "";
 };
 
 //toggles start button text and event listener to start/stop the game
@@ -100,18 +135,22 @@ const addStyle = (cell, i, j) => {
   cell.style.width = "10px";
   cell.style.float = "left";
   cell.alive = false;
+  cell.liveColor = liveColor;
+  cell.deadColor = deadColor;
   cell.id = `${i / 10}a${j / 10}`;
-  cell.style.backgroundColor = "green";
+  cell.style.backgroundColor = cell.deadColor;
 };
 
 //event listener function for changing a cell with a single click
 const clickBit = cell => {
   if (cell.alive === false && !isRunning) {
     cell.alive = true;
-    cell.style.backgroundColor = "yellow";
+    cell.liveColor = liveColor;
+    cell.style.backgroundColor = cell.liveColor;
   } else if (cell.alive === true && !isRunning) {
     cell.alive = false;
-    cell.style.backgroundColor = "green";
+    cell.deadColor = deadColor;
+    cell.style.backgroundColor = cell.deadColor;
   }
 };
 
@@ -119,10 +158,12 @@ const clickBit = cell => {
 const mouseOver = cell => {
   if (keydown === 67 && !cell.alive && !isRunning) {
     cell.alive = true;
-    cell.style.backgroundColor = "yellow";
+    cell.liveColor = liveColor;
+    cell.style.backgroundColor = cell.liveColor;
   } else if (keydown === 88 && cell.alive && !isRunning) {
     cell.alive = false;
-    cell.style.backgroundColor = "green";
+    cell.deadColor = deadColor;
+    cell.style.backgroundColor = cell.deadColor;
   }
 };
 
@@ -144,44 +185,75 @@ const loop = () => {
     for (var j = 0; j < boardSize[0]; j++) {
       let cell = document.getElementById(`${i}a${j}`);
       if (cell.alive) {
-        if (tempBoard[i][j] - 1 < 2) {
+        if (tempBoard[i][j][1] < 2) {
           cell.alive = false;
-          cell.style.backgroundColor = "green";
-        } else if (tempBoard[i][j] - 1 > 3) {
+          cell.style.backgroundColor = cell.deadColor;
+        } else if (tempBoard[i][j][1] > 3) {
           cell.alive = false;
-          cell.style.backgroundColor = "green";
-        } else if (tempBoard[i][j] === 2 || tempBoard[i][j] === 3) {
+          cell.style.backgroundColor = cell.deadColor;
+        } else if (tempBoard[i][j][1] === 2 || tempBoard[i][j][1] === 3) {
           cell.alive = true;
-          cell.style.backgroundColor = "yellow";
+          cell.liveColor = tempBoard[i][j][0];
+          cell.style.backgroundColor = cell.liveColor;
         }
       } else {
-        if (tempBoard[i][j] === 3) {
+        if (tempBoard[i][j][1] === 3) {
           cell.alive = true;
-          cell.style.backgroundColor = "yellow";
+          cell.liveColor = tempBoard[i][j][0];
+          cell.style.backgroundColor = cell.liveColor;
         }
       }
     }
   }
 };
 
-//checks and tallies all alive neighbors, including itself
-//If there's a way to exclude specific values from the conditional, like '... !== null && (dx !== 0 && dy !== 0)', let me know.
-//That looks like it should work, but didn't.
+//checks and tallies all alive neighbors and gets most common neighbor color
 const checkNeighbors = (i, j) => {
   let result = 0;
+  let colors = [];
+  let count = {};
   for (let dx = -1; dx <= 1; dx++) {
     for (let dy = -1; dy <= 1; dy++) {
-      if (
-        document.getElementById(`${i + dx}a${j + dy}`) !== null
-        // && (dx !== 0 && dy !== 0)
-      ) {
-        if (document.getElementById(`${i + dx}a${j + dy}`).alive) {
-          result += 1;
+      //excludes itself
+      if (!(dx === 0 && dy === 0)) {
+        if (document.getElementById(`${i + dx}a${j + dy}`) !== null) {
+          if (document.getElementById(`${i + dx}a${j + dy}`).alive) {
+            colors.push(
+              document.getElementById(`${i + dx}a${j + dy}`).liveColor
+            );
+            result += 1;
+          }
         }
       }
     }
   }
-  return result;
+  //takes the count object, gets a count of how many times each color appears as a neighbor, and reduces that to an array of [most common neighbor, total neighbors]
+  count = reduceCount(countUpDuplicates(count, colors), result);
+  return count;
 };
 
-//give speed up option, and allow changes to live/dead cells
+//Gets a count for how many times each color appears as a neighbor, by color. {purple: 3, grey: 1, #228B22: 2}
+const countUpDuplicates = (count, colors) => {
+  for (let i = 0; i < colors.length; i++) {
+    if (count[colors[i]]) {
+      count[colors[i]] += 1;
+    } else {
+      count[colors[i]] = 1;
+    }
+  }
+  return count;
+};
+
+//reduces the count object to an array of: [maximum counted color, number of neighbors]
+const reduceCount = (count, result) => {
+  if (Object.keys(count).length === 1) {
+    count = [Object.keys(count)[0], result];
+  } else if (Object.keys(count).length !== 0) {
+    count = Object.keys(count).reduce(
+      (a, b) => (count[a] > count[b] ? [a, result] : [b, result])
+    );
+  } else {
+    count = [deadColor, 0];
+  }
+  return count;
+};
